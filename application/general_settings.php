d1dfd9b39fec7bf8c593a9781c0534548dec09b8
Enock <38904828+enockkim@users.noreply.github.com>
2022-07-27 17:12:12 +0300

    settings

--- application/general_settings.php
+++ application/general_settings.php
@@ -14,14 +14,14 @@
       </h1>
       <ol class="breadcrumb">
         <li><a href="dashboard.php?id=<?php echo $_SESSION['tid']; ?>"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active"> <a href="loan_settings.php?id=<?php echo $_SESSION['tid']; ?>">Loan Settings</a></li>
+        <li class="active"> <a href="general_settings.php?id=<?php echo $_SESSION['tid']; ?>">General Settings</a></li>
         <!-- <li class="active">Add</li> -->
       </ol>
     </section>
 	
 	
     <section class="content">
-		<?php include("include/loan_settings_data.php"); ?>
+		<?php include("include/general_settings_data.php"); ?>
 	</section>
 </div>	
 
