6f3ec58976636ae63e8030fe6c1a99a5d384f637
Enock <38904828+enockkim@users.noreply.github.com>
2022-08-01 15:18:36 +0300

    t

--- application/include/newloans_data.php
+++ application/include/newloans_data.php
@@ -10,7 +10,7 @@
 					data: 'loanCode=' + loanCode
 				}).done(function(loanCode){
 					console.log(loanCode);
-					loanType = loanType.toString();
+					loanType = JSON.parse(loanType);
 					// $('#subcountyId').empty();
 					loanType.forEach(function(loanType){
 						document.getElementById("interestRate").value = loanType.interestRate;
